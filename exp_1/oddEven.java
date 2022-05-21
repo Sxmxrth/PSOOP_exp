0cf5e80a2cf138b7e55de3cb2cfa4c012d9ee097
Samarth Gupta <samarthgupta1202@gmail.com>
2022-05-21 11:43:42 +0530

    odd even file updated

--- exp_1/oddEven.java
+++ exp_1/oddEven.java
@@ -4,28 +4,55 @@ public class oddEven{
     public static void Odd_even(int a , int b){
         int odd=0;
         int even=0;
-        for (int i = a; i <=b ; i++) {
+        if(a%2==0){
+            System.out.println("even:");
+        }
+        else{
+            System.out.println("odd:");
+        }
+        for (int i = a; i <=b ; i+=2) {
             if(i%2==0){
                 even++;
-                System.out.println("even="+i);
+                System.out.print(i+" ");
             }
+
             else{
                 odd++;
-                System.out.println("odd="+i);
+                System.out.print(i+" ");
             }
 
         }
+        System.out.println("");
+        if((a+1)%2==0){
+            System.out.println("even:");
+        }
+        else{
+            System.out.println("odd:");
+        }
+        for (int i = a+1; i <=b ; i+=2) {
+            if(i%2==0){
+                even++;
+                System.out.print(i+" ");
+            }
+            else{
+                odd++;
+                System.out.print(i+" ");
+            }
+
+        }
+        System.out.println("");
         System.out.println("the number of even number in the range are="+even);
         System.out.println("the number of odd numbers in tge range are="+odd);
 
-        for (int i = 0; i <=100 ; i++) {
-            if(i%2==0){
-                System.out.println("even="+i);
-            }
-            else{
-                System.out.println("odd="+i);
-            }
+        System.out.println("odd:");
+        for (int i = 1; i <=100 ; i+=2) {
+            System.out.print(i+" ");
 
+        }
+        System.out.println("");
+        System.out.println("even:");
+        for (int i = 2; i <=100 ; i+=2) {
+            System.out.print(i+" ");
         }
     }
 }
