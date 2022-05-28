a94f3517571d3b93cf7038bac19dd4c651e40c00
Samarth Gupta <samarthgupta1202@gmail.com>
2022-05-28 11:50:13 +0530

    exp 2 added

---
+++ exp_2/EmployeeDemo.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+class Employee{
+    private String empName,empId;
+    private float salary;
+
+    Employee(){
+        empName="Samarth";
+        empId="E202023";
+        salary=15000f;
+    }
+    public String getName(){
+        return empName;
+    }
+    public String getId(){
+        return empId;
+    }
+    public float getSalary(){
+        return salary;
+    }
+    public void setName(String newName) {
+        this.empName = newName;
+    }
+    public void setId(String newId){
+        this.empId = newId;
+    }
+    public void setSalary(int newSal){
+        this.salary = newSal;
+    }
+
+    public void display(String a, String b , float c){
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+
+
+}
+
+public class EmployeeDemo{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number of employees=");
+        int n= sc.nextInt();
+        Employee[] arr =new Employee[n];
+        for(int i = 0; i<n ; i++){
+            arr[i]=new Employee();
+            String newName=sc.next();
+            String newId=sc.next();
+            int newSal=sc.nextInt();
+            //Employee emp= new Employee();
+            arr[i].setName(newName);
+            arr[i].setId(newId);
+            arr[i].setSalary(newSal);
+
+
+
+            String a=arr[i].getName();
+            String b=arr[i].getId();
+            float c=arr[i].getSalary();
+
+            arr[i].display(a,b,c);
+        }
+
+
+    }
+}
