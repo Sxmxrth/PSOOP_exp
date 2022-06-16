f343b9a6044ddf6d438714422e16bd4b55c2f738
Samarth Gupta <samarthgupta1202@gmail.com>
2022-06-16 21:02:05 +0530

    updated movies.java

--- exp_4/movies.java
+++ exp_4/movies.java
@@ -8,29 +8,30 @@ public class movies {
         Movies[] arr = new Movies[n];
 
         for (int i = 0; i <n ; i++) {
-            arr[i] = new Movies();
-            arr[i].info();
-
-
+            System.out.print("Enter the name of the movie: ");
+            String name = sc.nextLine();
+            System.out.print("enter the type of movie: ");
+            String type = sc.nextLine();
+            System.out.print("enter the Heros name: ");
+            String hero = sc.nextLine();
+            System.out.print("enter the Heroines name: ");
+            String heroine = sc.nextLine();
+            System.out.print("enter the budget: ");
+            double budget = sc.nextDouble();
+            arr[i] = new Movies(name, type, hero, heroine, budget);
         }
     }
 }
 
 class Movies{
-    Scanner sc = new Scanner(System.in);
-    void info(){
-        System.out.print("Enter the name of the movie: ");
-        String name = sc.nextLine();
-        System.out.print("enter the type of movie: ");
-        String type = sc.nextLine();
-        System.out.print("enter the Heros name: ");
-        String hero = sc.nextLine();
-        System.out.print("enter the Heroines name: ");
-        String heroine = sc.nextLine();
-        System.out.print("enter the budget: ");
-        double budget = sc.nextDouble();
-
-
+    String name,type, hero, heroine;
+    double budget;
+    Movies(String name,String type,String hero,String heroine,double budget){
+        this.name=name;
+        this.type=type;
+        this.hero=hero;
+        this.heroine=heroine;
+        this.budget=budget;
     }
 
 }
