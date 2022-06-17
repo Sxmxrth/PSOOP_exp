da228a92b2af2a0189410eb08376a730b4813527
Samarth Gupta <samarthgupta1202@gmail.com>
2022-06-17 12:15:31 +0530

    exp5 updated

--- exp_5/production.java
+++ exp_5/production.java
@@ -1,5 +1,80 @@
 public class production {
     public static void main(String[] args) {
-        
+        Play p1 = new Play();
+        Play p2 = new Play();
+        Play p3 = new Play();
+
+        Musical m1 = new Musical("Samarth","Naman");
+        Musical m2 = new Musical("abc", "xyz");
+
+        System.out.println(m1.getComposer());
+        System.out.println(m1.getLyricist());
+        //Musical m2 = new Musical();
+
+
+
     }
+}
+
+class Production{
+    private String title, director, writer;
+    Production(){
+        this.title="Lion King";
+        this.director="John Favreau";
+        this.writer="Sam";
+    }
+
+    public String toString(){
+        return title + " " + director + " " + writer;
+    }
+
+    String getTitle(){
+        return title;
+    }
+    String getDirector(){
+        return director;
+    }
+
+    String getWriter(){
+        return writer;
+    }
+
+    void setTitle(String newTitle){
+        this.title = newTitle;
+    }
+    void setDirector(String newDirector){
+        this.director = newDirector;
+    }
+
+    void setWriter(String newWriter){
+        this.writer = newWriter;
+    }
+}
+
+class Play extends Production{
+    int performances=0;
+    void play(){
+        performances++;
+    }
+
+
+}
+
+class Musical extends Play{
+    private String composer, lyricist;
+
+    Musical(String newComposer, String newLyricist){
+        this.composer = newComposer;
+        this.lyricist = newLyricist;
+    }
+
+    String getComposer(){
+        return composer;
+    }
+
+    String getLyricist(){
+        return lyricist;
+    }
+
+
 }
