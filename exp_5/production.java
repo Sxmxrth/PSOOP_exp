417052c4e21446aa042922833ccc7e14eefbe137
Samarth Gupta <samarthgupta1202@gmail.com>
2022-06-17 13:11:32 +0530

    exp5 updated

--- exp_5/production.java
+++ exp_5/production.java
@@ -1,5 +1,3 @@
-import jdk.swing.interop.SwingInterOpUtils;
-
 import java.util.Scanner;
 
 public class production {
@@ -7,11 +5,53 @@ public class production {
         Scanner sc= new Scanner(System.in);
         System.out.print("enter the number of plays: ");
         int n = sc.nextInt();
+        System.out.println("enter the number of musical");
+        int m = sc.nextInt();
         Play[] arr1 = new Play[n];
+        Musical[] arr2 = new Musical[m];
         for (int i = 0; i <n ; i++) {
             arr1[i] = new Play();
+            sc.skip("\\R");
+            System.out.print("enter the name of the Play: ");
+            String name = sc.nextLine();
+            arr1[i].setTitle(name);
+            System.out.print("enter the name of the Director: ");
+            String director=sc.nextLine();
+            System.out.print("enter the name of the Writer: ");
+            arr1[i].setDirector(director);
+            String writer = sc.nextLine();
+            arr1[i].setWriter(writer);
+
             arr1[i].play();
+            System.out.println(arr1[i]);
         }
+        System.out.println("total cost of the seats for play are: "+(n*500));
+
+        for (int i = 0; i <m ; i++) {
+            arr2[i] = new Musical();
+            System.out.print("enter the name of the Musical: ");
+            String name = sc.nextLine();
+            arr2[i].setTitle(name);
+            System.out.print("enter the name of the Director: ");
+            String director=sc.nextLine();
+            arr2[i].setDirector(director);
+            System.out.print("enter the name of the Writer: ");
+            String writer = sc.nextLine();
+            arr2[i].setWriter(writer);
+            System.out.print("enter the name of the composer: ");
+            String composer= sc.nextLine();
+            arr2[i].setComposer(composer);
+            System.out.println("enter the name of the lyricist");
+            String lyricist = sc.nextLine();
+            arr2[i].setLyricist();
+
+
+            System.out.println(arr2[i]);
+
+
+
+        }
+
     }
 }
 
@@ -55,21 +95,13 @@ class Play extends Production{
     void play(){
         performances++;
     }
-
     public String toString(){
-        return super.getTitle() + " " + super.getDirector() + " "+ super.getWriter();
+        return performances+ ", " + super.getTitle() + ", " + super.getDirector() + ", "+ super.getWriter();
     }
-
-
 }
 
 class Musical extends Play{
     private String composer, lyricist;
-
-    Musical(String newComposer, String newLyricist){
-        this.composer = newComposer;
-        this.lyricist = newLyricist;
-    }
 
     String getComposer(){
         return composer;
@@ -79,5 +111,16 @@ class Musical extends Play{
         return lyricist;
     }
 
+    void setComposer(String newComposer){
+        this.composer=newComposer;
+    }
+
+    void setLyricist(String newLyricist){
+        this.lyricist=newLyricist;
+    }
+
+    public String toString(){
+        return super.getTitle()+" "+ super.getDirector()+" "+ super.getWriter()+" "+setComposer()+" "+ setLyricist();
+    }
 
 }
