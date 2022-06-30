aef782d889db47b685726aae1a8e09e8aacd59ea
Samarth Gupta <samarthgupta1202@gmail.com>
2022-06-30 20:45:48 +0530

    exp 7b added

---
+++ exp_7/marks.java
@@ -0,0 +1,21 @@
+public class marks {
+    public static void main(String[] args) {
+
+    }
+}
+
+abstract class Marks{
+    abstract void getPercentage();
+}
+
+class A extends Marks{
+    void getPercentage(){
+
+    }
+}
+
+class B extends Marks{
+    void getPercentage(){
+
+    }
+}
