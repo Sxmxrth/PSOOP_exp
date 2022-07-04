ed345c6bb34175a883d738217e0d1adb65b8dd64
Samarth Gupta <samarthgupta1202@gmail.com>
2022-07-04 17:11:01 +0530

    interfaces.java updated

--- exp_8/interfaces.java
+++ exp_8/interfaces.java
@@ -1,8 +1,7 @@
 import java.util.*;
 public class interfaces{
-    public static void main(String args[]){
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-
 
         do{
             System.out.println("1. Cylinder");
